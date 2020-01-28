0abc27b982375c01ccc4ac096d3b5344475de01a
Ariel <ariel@netcredbrasil.com.br>
2020-01-28 13:32:35 -0300

    Atualização das tarefas para serem realizadas

--- Modal_Library.v
+++ Modal_Library.v
@@ -1,26 +1,30 @@
+(* Biblioteca construída com o enfoque de 
+auxiliar o ensino sobre lógica proposicional clássica
+a partir da pesquisa realizada por Karina && Machado *)
 Require Import Propositional_Library.
 
 (*Rever o model checking para saber se não vai dar conflito
 com o inductive da Propositional_Library*)
 
 Inductive formula_Modal : Set :=
-    | Lit          : nat -> formula_Modal
-    | Neg          : formula_Modal -> formula_Modal
+    | Lit_Modal          : nat -> formula_Modal
+    | Neg_Modal          : formula_Modal -> formula_Modal
     | Box          : formula_Modal -> formula_Modal
     | Dia          : formula_Modal -> formula_Modal
-    | And          : formula_Modal -> formula_Modal -> formula_Modal
-    | Or           : formula_Modal -> formula_Modal -> formula_Modal
-    | Implies      : formula_Modal -> formula_Modal -> formula_Modal 
-    | formula_Prop : formula
+    | And_Modal          : formula_Modal -> formula_Modal -> formula_Modal
+    | Or_Modal           : formula_Modal -> formula_Modal -> formula_Modal
+    | Implies_Modal      : formula_Modal -> formula_Modal -> formula_Modal 
+    (* | formula_Prop : formula *)
 .
-
-Notation " X .-> Y "  := (Implies X Y) (at level 13, right associativity).
-Notation " X .\/ Y "  := (Or X Y)      (at level 12, left associativity).
-Notation " X ./\ Y"   := (And X Y)     (at level 11, left associativity).
-Notation " .~ X "     := (Neg X)       (at level 10, right associativity).
-Notation " .[] X "    := (Box X)       (at level 'x', right associativity).
-Notation " .<> X "    := (Dia X)       (at level 'x', right associativity).
-Notation " # X "      := (Lit X)       (at level 1, no associativity).
+(* Tentar ver como colocar se uma fórmula é modal ou proposicional *)
+(* Ver a precedencia dos operadores unários *)
+Notation " X .-> Y "  := (Implies_Modal X Y) (at level 13, right associativity).
+Notation " X .\/ Y "  := (Or_Modal X Y)      (at level 12, left associativity).
+Notation " X ./\ Y"   := (And_Modal X Y)     (at level 11, left associativity).
+Notation " .~ X "     := (Neg_Modal X)       (at level 10, right associativity).
+Notation " .[] X "    := (Box X)             (at level 10, right associativity).
+Notation " .<> X "    := (Dia X)             (at level 10, right associativity).
+Notation " # X "      := (Lit_Modal X)       (at level 1, no associativity).
 
 (* Calcula o tamanho de uma fórmula com base na lógica modal *)
 Fixpoint size (f:formula_Modal) : nat :=
@@ -40,3 +44,15 @@ Definition ex3_modal := .~.~ .<> #0 .-> .[] #1.
     
 Check size ex1.
 Compute size ex1.
+
+(* Começar a construção de um modelo *)
+(* Começar o desenvolvimento de mundos *)
+(* Rever as anotações criadas pela Kaqui *)
+(* Uma proposição vai ter uma lista de mundos *)
+(* Ver como funciona a construção do Ltac *)
+(* Saber como construir uma fórmula modal *)
+(* Modelar os diferentes sistemas (K, B, D, T, 4, 5...) *)
+(* Diferentes propriedades de cada um modelo, dessa forma
+será visto cada uma das restrições. Página 29 *)
+(* Provar cada metapropriedade do capítulo 2.4 *)
+(* Regras: De Morgan, Necessitação e Axiomas *)
