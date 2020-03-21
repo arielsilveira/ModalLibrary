1cd879867e7c5493ee83578bf2527a4e73104c31
Ariel Agne <ariel.agne95@gmail.com>
2020-03-21 13:13:38 -0300

    Melhoria de vizualização de codigo

--- Modal_Library.v
+++ Modal_Library.v
@@ -1,15 +1,15 @@
-(*  Name: Ariel Agne da Silveira
+(*  
+    Name: Ariel Agne da Silveira
+
+    Advisor: Karina Girardi Roggia
+
+    Minion: Miguel
 
     <Modal Logic Library>
-
+    Description:
 *)
 
-Require Import Arith List ListSet Classical Logic.
-
-(* Parameter A : Type.
-Parameter le : A -> Prop. 
-Infix "≤" := le : order_scope.
-*)
+Require Import Arith List ListSet Classical Logic Nat Notations.
 
 Inductive formulaModal : Set :=
     | Lit          : nat -> formulaModal
@@ -20,16 +20,6 @@ Inductive formulaModal : Set :=
     | Or           : formulaModal -> formulaModal -> formulaModal
     | Implies      : formulaModal -> formulaModal -> formulaModal 
 .
-
-(* Tentar ver como colocar se uma fórmula é modal ou proposicional *)
-(* Ver a precedencia dos operadores unários *)
-Notation " X .-> Y "  := (Implies X Y) (at level 13, right associativity).
-Notation " X .\/ Y "  := (Or X Y)      (at level 12, left associativity).
-Notation " X ./\ Y"   := (And X Y)     (at level 11, left associativity).
-Notation " .~ X "     := (Neg X)       (at level 10, right associativity).
-Notation " .[] X "    := (Box X)       (at level 10, right associativity).
-Notation " .<> X "    := (Dia X)       (at level 10, right associativity).
-Notation " # X "      := (Lit X)       (at level 1, no associativity).
 
 (* Calcula o tamanho de uma fórmula com base na lógica modal *)
 Fixpoint sizeModal (f:formulaModal) : nat :=
@@ -44,23 +34,15 @@ Fixpoint sizeModal (f:formulaModal) : nat :=
 end.
 
 Fixpoint literals (f:formulaModal) : set nat :=
-match f with 
-| Lit      x     => set_add eq_nat_dec x (empty_set nat)
-| Dia      p1    => literals p1
-| Box      p1    => literals p1
-| Neg      p1    => literals p1
-| And      p1 p2 => set_union eq_nat_dec (literals p1) (literals p2)
-| Or       p1 p2 => set_union eq_nat_dec (literals p1) (literals p2)
-| Implies  p1 p2 => set_union eq_nat_dec (literals p1) (literals p2) 
+    match f with 
+    | Lit      x     => set_add eq_nat_dec x (empty_set nat)
+    | Dia      p1    => literals p1
+    | Box      p1    => literals p1
+    | Neg      p1    => literals p1
+    | And      p1 p2 => set_union eq_nat_dec (literals p1) (literals p2)
+    | Or       p1 p2 => set_union eq_nat_dec (literals p1) (literals p2)
+    | Implies  p1 p2 => set_union eq_nat_dec (literals p1) (literals p2) 
 end.
-
-(* 
-    Constroi uma nova notação para listas
-*)
-Notation "[ ]" := nil.
-Notation "x :: l" := (cons x l)
-                     (at level 60, right associativity).
-Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..).
 
 Inductive World : Set :=
     | w : nat -> World
@@ -71,31 +53,28 @@ Inductive Relation : Prop :=
     | r : World -> World -> Relation
 .
 
+(* -- New notation -- *)
+Notation " X .-> Y "  := (Implies X Y) (at level 13, right associativity).
+Notation " X .\/ Y "  := (Or X Y)      (at level 12, left associativity).
+Notation " X ./\ Y"   := (And X Y)     (at level 11, left associativity).
+Notation " .~ X "     := (Neg X)       (at level 10, right associativity).
+Notation " .[] X "    := (Box X)       (at level 10, right associativity).
+Notation " .<> X "    := (Dia X)       (at level 10, right associativity).
+Notation " # X "      := (Lit X)       (at level 1, no associativity).
 
 Notation "w # X" := (w X) (at level 1, no associativity).
 Notation "x $ y" := (r x y) (at level 1, no associativity).
 
-(* Definition verify_relation (W : World) (R : Relation) : Prop := forall . *)
+Notation "[ ]" := nil.
+Notation "x :: l" := (cons x l)
+                     (at level 60, right associativity).
+Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..).
 
-Fixpoint eqb (n m : nat) : bool :=
-  match n with
-  | O => match m with
-         | O => true
-         | S m' => false
-         end
-  | S n' => match m with
-            | O => false
-            | S m' => eqb n' m'
-            end
-  end.
-
-Notation "x && y" := (andb x y).
-Notation "x =? y" := (eqb x y) (at level 70).
 
 Fixpoint eqb_World (x x' : World): bool :=
   match x with
     | w n => match x' with 
-              | w n' => if eqb n n' then true else false
+              | w n' => if  n =? n' then true else false
             end
   end.
 
@@ -106,12 +85,12 @@ Fixpoint In_World (x: World) (l: list World): bool :=
                 else In_World x t
   end.
 
-Fixpoint teste (Worlds : list World) (Relations : list (World * World)) : list (World * World) :=
+Fixpoint remove_invalidate (Worlds : list World) (Relations : list (World * World)) : list (World * World) :=
 match Relations with
     | nil => nil 
-    | h :: t => if (In_World (fst h) Worlds) && (In_World (snd h) Worlds) 
-                          then ((fst h, snd h) :: nil) ++ teste Worlds t
-                          else teste Worlds t
+    | h :: t => if andb (In_World (fst h) Worlds) (In_World (snd h) Worlds) 
+                          then ((fst h, snd h) :: nil) ++ remove_invalidate Worlds t
+                          else remove_invalidate Worlds t
 end.
 
 Fixpoint pair_to_relation (l : list (World * World)) : list Relation :=
@@ -121,12 +100,12 @@ Fixpoint pair_to_relation (l : list (World * World)) : list Relation :=
   end.
 
 Definition validate_relation (Worlds : list World) (lw : list (World * World)) : list Relation := 
-pair_to_relation (teste Worlds lw).
+pair_to_relation (remove_invalidate Worlds lw).
 
 
 Record Frame : Type := frame{
     W : list World; (*Recebe uma lista de mundos*)
-    R : (validate_relation W Relation)->list Relation; (*Recebe uma lista de pares ordenados*)
+    R : list Relation; (*Recebe uma lista de pares ordenados*)
 }.
 
 Record Model : Type :={
